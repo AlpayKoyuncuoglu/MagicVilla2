cb134f50b2807f9f39d61cb3305c7b6fe9d955f9
IOM\alpay.koyuncuoglu <alpay.koyuncuoglu@ist-pay.com>
2023-03-29 14:45:44 +0300

    second branch

--- MagicVilla-master/MagicVilla_VillaAPI/Program.cs
+++ MagicVilla-master/MagicVilla_VillaAPI/Program.cs
@@ -1,11 +1,12 @@
+﻿using MagicVilla_VillaAPI.Logging;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().
-    WriteTo.File("log/villaLogs3.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().
+//    WriteTo.File("log/villaLogs3.txt", rollingInterval: RollingInterval.Day).CreateLogger();
 
 builder.Host.UseSerilog();
 
@@ -13,6 +14,18 @@ builder.Services.AddControllers(option => { option.ReturnHttpNotAcceptable = tru
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//maximum and longest time is singleton 
+//works when applicaiton starts and every application request 
+
+//addscoped
+//for every request
+
+//add transient
+//every time that object is accessed, even one request
+//is accessed like ten request it will create ten different objects and assign that where it's needed
+
+//uygulama çalıştığında baştan sona bir kez loglama yapması için singelton kullanılmakta
+//builder.Services.AddSingleton<ILogging, LoggingV2>();
 
 var app = builder.Build();
 
